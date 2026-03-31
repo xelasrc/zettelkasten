49e0dd462cef8dc207a14406bbfb4d2cc0f3a247
xelasrc <amwells2003@gmail.com>
2026-04-01 02:14:04 +1300

    fix: login not routing instantly

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <SignIn />
+      <SignIn forceRedirectUrl="/notes" />
     </div>
   )
 }
